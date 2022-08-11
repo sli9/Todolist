d34449b10856a457eefa6497b35ee485f5c501f0
Sil9 <sil9gosy@mail.ru>
2022-08-11 21:51:24 +0300

    toolkit for tasks-reducer.ts

--- src/features/TodolistsList/todolists-reducer.ts
+++ src/features/TodolistsList/todolists-reducer.ts
@@ -1,42 +1,44 @@
 import {todolistsAPI, TodolistType} from '../../api/todolists-api'
 import {Dispatch} from 'redux'
-import {RequestStatusType, SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from '../../app/app-reducer'
+import {RequestStatusType, setAppStatusAC} from '../../app/app-reducer'
 import {handleServerNetworkError} from '../../utils/error-utils'
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import {createSlice, PayloadAction} from '@reduxjs/toolkit'
 
 const initialState: Array<TodolistDomainType> = []
 
-const  slice = createSlice({
+const slice = createSlice({
     name: 'todolists',
     initialState: initialState,
     reducers: {
-        removeTodolistAC(state, action: PayloadAction<{id: string}>) {
-          return  state.filter(tl => tl.id !== action.payload.id)
+        removeTodolistAC(state, action: PayloadAction<{ id: string }>) {
+            return state.filter(tl => tl.id !== action.payload.id)
         },
-        addTodolistAC(state, action: PayloadAction<{todolist: TodolistType}>) {
+        addTodolistAC(state, action: PayloadAction<{ todolist: TodolistType }>) {
             state.unshift({...action.payload.todolist, filter: 'all', entityStatus: 'idle'})
         },
-        changeTodolistTitleAC(state, action: PayloadAction<{id: string, title: string}>) {
+        changeTodolistTitleAC(state, action: PayloadAction<{ id: string, title: string }>) {
             const index = state.findIndex(tl => tl.id === action.payload.id)
             state[index].title = action.payload.title
         },
-        changeTodolistFilterAC(state, action: PayloadAction<{id: string, filter: FilterValuesType}>) {
+        changeTodolistFilterAC(state, action: PayloadAction<{ id: string, filter: FilterValuesType }>) {
             const index = state.findIndex(tl => tl.id === action.payload.id)
-            state[index].title = action.payload.filter
+            state[index].filter = action.payload.filter
         },
-        changeTodolistEntityStatusAC(state, action: PayloadAction<{id: string, status: RequestStatusType}>) {
+        changeTodolistEntityStatusAC(state, action: PayloadAction<{ id: string, status: RequestStatusType }>) {
             const index = state.findIndex(tl => tl.id === action.payload.id)
             state[index].title = action.payload.status
         },
-        setTodolistsAC(state, action: PayloadAction<{todolists: Array<TodolistType>}>) {
+        setTodolistsAC(state, action: PayloadAction<{ todolists: Array<TodolistType> }>) {
             return action.payload.todolists.map(tl => ({...tl, filter: 'all', entityStatus: 'idle'}))
         },
     }
 })
 
 export const todolistsReducer = slice.reducer
-export const {removeTodolistAC, addTodolistAC, changeTodolistTitleAC,
-    changeTodolistFilterAC, changeTodolistEntityStatusAC, setTodolistsAC} = slice.actions
+export const {
+    removeTodolistAC, addTodolistAC, changeTodolistTitleAC,
+    changeTodolistFilterAC, changeTodolistEntityStatusAC, setTodolistsAC
+} = slice.actions
 
 // thunks
 export const fetchTodolistsTC = () => {
@@ -86,8 +88,6 @@ export const changeTodolistTitleTC = (id: string, title: string) => {
 }
 
 // types
-export type AddTodolistActionType = ReturnType<typeof addTodolistAC>;
-export type RemoveTodolistActionType = ReturnType<typeof removeTodolistAC>;
 export type SetTodolistsActionType = ReturnType<typeof setTodolistsAC>;
 
 export type FilterValuesType = 'all' | 'active' | 'completed';
