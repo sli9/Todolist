1afc42520f7bfd4f8c2447265835e1fe9c7d64b6
Sil9 <sil9gosy@mail.ru>
2022-10-22 16:19:44 +0300

    Updates

---
+++ precache-manifest.b8ea59379a3c0775b2b5b3e4a1f496b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3d157cc7e5d5818d86bd16ce169441",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "5ac9a5832c9cc4aa46f7",
+    "url": "/Todolist/static/css/main.6da83bb5.chunk.css"
+  },
+  {
+    "revision": "9e10c19b36a1723714e0",
+    "url": "/Todolist/static/js/2.889a3dc5.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.889a3dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac9a5832c9cc4aa46f7",
+    "url": "/Todolist/static/js/main.d2af0a49.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
